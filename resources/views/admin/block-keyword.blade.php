61e289e5ddd6743d5bc92d2743592be0220ecee5
功志 黃 <billy32073@gmail.com>
2017-10-04 23:14:08 +0800

    add somthing

---
+++ resources/views/admin/block-keyword.blade.php
@@ -0,0 +1,16 @@
+@extends('admin.layout')
+
+@section('content')
+
+  <div id="tableController" class="table-responsive"></div>
+
+@endsection
+@section('foot')
+
+  <div class="row">
+    <div class="col-sm-6 col-sm-offset-3">
+      <script src="/js/block-keyword.js"></script>
+    </div>
+  </div>
+
+@endsection
