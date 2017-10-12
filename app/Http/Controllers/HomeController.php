1a6c25587731ad4971d5a4eda9aedc39e1028f41
Karsten Huang/WHQ/Wistron <Karsten_Huang@wistron.com>
2017-10-12 16:33:53 +0800

    add test code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,4 +22,41 @@ class HomeController extends Controller
         return view('about');
     }
 
+
+
+    public function test(){
+
+        $myfile = fopen("/home/vagrant/code/config/publisher.php", "w") or die("Unable to open file!");
+
+        $user = "admin";
+        $password = "password";
+        $app_id = "1824467507820475";
+        $app_secret = "d6f47b6007b5590c1a274e5614f98615";
+        $default_graph_version = "v2.8";
+        $fb_page_token = "EAAZA7VZCG1N7sBAEL9uM9DA6v9VqbF3Yry2YJEt3JxtG5DEHJPau2MmXJ4O6tGg8ihKUIxWarDnJzBCeuZCpWjTCqtTMsPGTcDiSUrtmGUKVmRZBkPqfBOjaRusZCm60LFu5xsFeaK5ufFf5Q8Uosx7vZCO0GNJ0pSbLeGKbLayQZDZD";
+        $recaptcha_key = "6LfXqgATAAAAACzwvvdV59zzxpdFOkWDSsa0331s";
+        $recaptcha_secret = "6LfXqgATAAAAALVnEIzAjJ67pGSe7sI1-yUoFZfu";
+
+        fwrite($myfile, "<?php return [\n    'user' => '");
+        fwrite($myfile, $user);
+        fwrite($myfile,"',\n    'password' => '");
+        fwrite($myfile, $password);
+        fwrite($myfile,"',\n    'fb_app_setting' > [\n        'app_id' => '");
+        fwrite($myfile, $app_id);
+        fwrite($myfile,"',\n        'app_secret' => '");
+        fwrite($myfile, $app_secret);
+        fwrite($myfile,"',\n        'default_graph_version' => '");
+        fwrite($myfile, $default_graph_version);
+        fwrite($myfile,"',\n    ],\n    'fb_page_token' => '");
+        fwrite($myfile, $fb_page_token);
+        fwrite($myfile,"',\n    'recaptcha_key' => '");
+        fwrite($myfile, $recaptcha_key);
+        fwrite($myfile,"',\n    'recaptcha_secret' => '");
+        fwrite($myfile, $recaptcha_secret);
+        fwrite($myfile,"',\n];");
+
+        fclose($myfile);
+        return view('maintain');
+
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,7 @@
 Route::get('/', 'HomeController@index');
 
 Route::get('/about', 'HomeController@about');
+Route::get('/test', 'HomeController@test');
 
 Route::post('/submit', 'PublisherController@submit');
 Route::post('/preview', 'ImageController@preview');
